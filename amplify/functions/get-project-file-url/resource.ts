0a7e61ea6f0addf450a79a96affbdcc625e4bcf5
hiroshige.negishi <hiroshige.negishi@tentspace.net>
2026-04-11 01:45:45 +0900

    update node runtime v24

--- amplify/functions/get-project-file-url/resource.ts
+++ amplify/functions/get-project-file-url/resource.ts
@@ -3,6 +3,6 @@ import { defineFunction } from "@aws-amplify/backend";
 export const getProjectFileUrl = defineFunction({
   name: "get-project-file-url",
   entry: "./handler.ts",
-  runtime: 22,
+  runtime: 24,
   timeoutSeconds: 10,
 });
